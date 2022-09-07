67c3adc5655c6ca95a9032dd1ae87b22b2c98de5
byaskew3 <byaskew3@gmail.com>
2022-09-06 19:24:42 -0600

    full lecture file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from config import Config
+
+#import blueprint
+from .auth.routes import auth
 
 app= Flask(__name__)
+
+# register blueprint
+app.register_blueprint(auth)
+
+app.config.from_object(Config)
 
 from . import routes
